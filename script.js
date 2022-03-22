373d6b6c462019b83b51b9ca4bd7083d75021b31
Isabel Terenzi <bel.terenzi@gmail.com>
2022-03-22 19:48:35 -0300

    adiciona mais requisitos

--- script.js
+++ script.js
@@ -2,26 +2,48 @@ let tarefa = '';
 
 const lista = document.getElementById('lista-tarefas');
 const botaoAdd = document.querySelector('#criar-tarefa');
+const botaoLimpar = document.querySelector('#apaga-tudo');
 
 botaoAdd.addEventListener('click', adicionaTarefa);
+botaoLimpar.addEventListener('click', limpaLista);
 
-//botaoLimparLista.addEventListener('click', apagaTudo);
 //botaoLimparCompletos.addEventListener('click', removeFinalizados);
 function criaTarefa(texto) {
-    const itemLista = document.createElement('li');
-    itemLista.innerText = texto;
-
-    lista.appendChild(itemLista);
+  const itemLista = document.createElement('li');
+  itemLista.innerText = texto;
+  itemLista.addEventListener('click', ficaCinza);
+  itemLista.addEventListener('dblclick', ficaRiscado);
+  lista.appendChild(itemLista);
 }
-
-
 
 function adicionaTarefa() {
-    tarefa = document.getElementById('texto-tarefa').value;
+  tarefa = document.getElementById('texto-tarefa').value;
 
-    localStorage.setItem(tarefa, '');
+  localStorage.setItem(tarefa, '');
 
-    document.getElementById('texto-tarefa').value = '';
+  document.getElementById('texto-tarefa').value = '';
 
-    criaTarefa(tarefa);
+  criaTarefa(tarefa);
 }
+
+function limpaLista() {
+  lista.innerHTML = '';
+  localStorage.clear();
+}
+
+function ficaCinza(event) {
+  event.target.classList.toggle('gray');
+}
+
+function ficaRiscado(event) {
+  event.target.classList.toggle('completed');
+}
+
+window.onload = function () {
+  if (localStorage.length > 0) {
+    for (let i = 0; i < localStorage.length; i += 1) {
+      let key = localStorage.key(i);
+      criaTarefa(tarefa);
+    }
+  }
+};
